063a60d00155bcc4d58770c0ba08381a16604ff8
Siddharth M <siddharth.m98@gmail.com>
2019-06-08 15:52:18 +0530

    All files till date

---
+++ includes/table/checkbox.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 2.11.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 09, 2016 at 06:59 AM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `checkbox`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table1`
+--
+
+CREATE TABLE IF NOT EXISTS `table1` (
+  `id` int(5) NOT NULL auto_increment,
+  `name` varchar(10) NOT NULL,
+  `city` varchar(10) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+
+--
+-- Dumping data for table `table1`
+--
+
+INSERT INTO `table1` (`id`, `name`, `city`) VALUES
+(25, 'sds', 'sdsd'),
+(26, 'test', 'test'),
+(24, 'sds', 'sdsa'),
+(22, 'skpatel', 'rajkot');
