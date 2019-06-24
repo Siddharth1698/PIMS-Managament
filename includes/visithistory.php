06b93a8b5d861fffcc3c10c1cc9877a66a15ebc9
Siddharth M <siddharth.m98@gmail.com>
2019-06-24 16:00:46 +0530

    Update visithistory.php

--- includes/visithistory.php
+++ includes/visithistory.php
@@ -21,6 +21,8 @@ $i =0 ;
     <tr>
       <th scope="col">#</th>
       <th scope="col">Visit History</th>
+      <th scope="col"></th>
+
     </tr>
   </thead>
   <tbody>
@@ -33,7 +35,10 @@ while($row = mysqli_fetch_array($select_user_query)){
 
       ?><tr>
       <th scope="row"><?php echo "$rowcount";  ?></th>
-          <td><a href="report.php?user_id=<?php echo $uid; ?>&current_appointment=<?php echo $current_appointment; ?>"> <?php echo "$current_appointment";  ?></a></td> </tr>  
+          <td><a href="report.php?user_id=<?php echo $uid; ?>&current_appointment=<?php echo $current_appointment; ?>"> <?php echo "$current_appointment";  ?></a></td>
+         <td> <a href="prescription.php?user_id=<?php echo "$user_id"?>&flag=0&post_id=0&admin_id=<?php echo "$admin_id" ?>&p_date=<?php echo "$p_date" ?>&current_appointment=<?php echo "$current_appointment"?> ">Prescription</a> </td>
+
+          </tr>  
      <?php   $rowcount = $rowcount - 1;
  }
 ?>
