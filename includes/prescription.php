a946023433a861494e8235a353ebd3868265eff8
Siddharth M <siddharth.m98@gmail.com>
2019-06-24 14:22:38 +0530

    medication display in prescription

--- includes/prescription.php
+++ includes/prescription.php
@@ -3,6 +3,7 @@
 ob_start();
  include "db.php";
  $user_id = $_GET["user_id"];
+ $p_date = $_GET["p_date"];
  $current_appointment = $_GET["current_appointment"];
 
 
@@ -66,10 +67,40 @@ $select_user_query = mysqli_query($connection, $sql);
 <p><span style="font-weight: 400;">Name</span><span style="font-weight: 400;">: <?php echo "$fname"; echo " "; echo "$lname";  ?></span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <span style="font-weight: 400;">Age</span><span style="font-weight: 400;">: <?php echo "$age";  ?> Years&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span><span style="font-weight: 400;">Gender</span><span style="font-weight: 400;">: <?php echo "$gender";  ?></span></p>
 <p>&nbsp;</p>
 <p><span style="font-weight: 400;">Medications</span><span style="font-weight: 400;">:</span></p>
-<p><span style="font-weight: 400;">Deigene</span> <span style="font-weight: 400;">1-1-1</span> <span style="font-weight: 400;">for 2 weeks</span></p>
-<p><span style="font-weight: 400;">Feigene</span> <span style="font-weight: 400;">1-0-1</span> <span style="font-weight: 400;">for 1 weeks</span></p>
-<p><span style="font-weight: 400;">Seigene</span> <span style="font-weight: 400;">1-1-0</span> <span style="font-weight: 400;">for 0.5 weeks</span></p>
-<p>&nbsp;</p>
+
+
+      <?php
+       $i = 1;
+
+$sql = "SELECT * FROM medication WHERE user_id='$user_id' AND p_date='$p_date'";
+$select_user_query = mysqli_query($connection, $sql);
+
+ while($row = mysqli_fetch_array($select_user_query)){
+ 
+    $inname = $row['name'];
+    $indet = $row['detail'];
+        $inmed = $row['med'];
+
+    
+ 
+
+
+
+?><tr>
+      <th scope="row"><?php echo "$i"; ?></th>
+      <td><?php echo "$inname";  ?></td>
+      <td><?php echo "$indet"; echo " ";  echo "for"; ?></td>
+       <td><?php echo "$inmed";  ?></td>
+      
+    </tr>
+
+    <?php
+    $i =$i + 1;
+}
+
+    ?>
+    
+
 <p><span style="font-weight: 400;">Final Diagnosis</span><span style="font-weight: 400;">: </span><em><span style="font-weight: 400;">In case ticked in the visit report</span></em><span style="font-weight: 400;"> &nbsp;</span></p>
 <p>&nbsp;</p>
 <p><span style="font-weight: 400;">Next review date</span><span style="font-weight: 400;">: dd/mm/yyyy</span></p></div>
